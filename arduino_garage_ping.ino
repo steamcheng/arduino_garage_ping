d0023329fed160fb2833af00d89f57973f3c0ce3
dwhitt <dwhitt@n1kx.net>
2016-10-21 08:31:11 -0400

    Updated reset function and pin modes for timeoute issue.

--- arduino_garage_ping.ino
+++ arduino_garage_ping.ino
@@ -18,14 +18,15 @@
 #define TRIGGER_PIN2  7   // Arduino pin tied to trigger pin on 2nd ultrasonic sensor.
 #define ECHO_PIN2     8   // Arduino pin tied to echo pin on 2nd ultrasonic sensor.
 
+#define redpin 10
+#define yellowpin  11
+#define greenpin  12
+
 #define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
-int redpin = 10;
-int yellowpin = 11;
-int greenpin = 12;
-int ping1;
-int ping2;
-int distance;
+int ping1;     // Holds distance returned by sensor 1
+int ping2;     // Holds distance returned by sensor 2
+int distance;  // Holds average distance reported by both sensors
 
 NewPing sonar1(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 NewPing sonar2(TRIGGER_PIN2, ECHO_PIN2, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
@@ -48,10 +49,11 @@ void setup() {
 }
 
 void loop() {
-  delay(40);  // Wait 40ms between pings (about 25 pings/sec). 29ms should be the shortest delay between pings.
-  ping1 = sonar1.ping_in();
-  ping2 = sonar2.ping_in();
-  distance = (ping1 + ping2)/2;
+  delay(50);  // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  ping1 = sonar1.ping_in();  // Get sensor 1 distance
+  ping2 = sonar2.ping_in();  // Get sensor 2 distance
+  distance = (ping1 + ping2)/2;  // Calculate average distance
+  /* Now some serial output for troubleshooting */
   Serial.print("Ping1: ");
   Serial.print(ping1); // Send ping, get distance in cm and print result (0 = outside set distance range)
   Serial.print("in    ");  
@@ -61,32 +63,39 @@ void loop() {
   Serial.print("Avg: ");
   Serial.print(distance);   
   Serial.println("in");  
+  /* End of serial troubleshooting data section.  */
 
-  if (distance > 24 || distance == 0) {
+  /* Check if sensor is timed out */
+  if (distance == 0){
+    resetSensors();
+  }
+
+  /* Check distance in several ranges and turn on appropriate lights. */
+  if (distance > 36 || distance == 0) {
     digitalWrite(greenpin, LOW);
     digitalWrite(yellowpin, LOW);     
     digitalWrite(redpin, LOW);
   }
   
-  if (distance > 18 && distance < 24) {
+  if (distance > 24 && distance < 36) {
     digitalWrite(greenpin, HIGH);
     digitalWrite(yellowpin, LOW);     
     digitalWrite(redpin, LOW);
   }
 
-  if (distance > 12 && distance <= 18) {
+  if (distance > 18 && distance <= 24) {
     digitalWrite(greenpin, HIGH);
     digitalWrite(yellowpin, HIGH);
     digitalWrite(redpin, LOW);  
   }  
   
-  if (distance > 9 && distance <= 12) {
+  if (distance > 12 && distance <= 18) {
     digitalWrite(greenpin, LOW);
     digitalWrite(yellowpin, HIGH);
     digitalWrite(redpin, HIGH);  
   }    
   
-if (distance <= 9 && distance != 0) {
+if (distance <= 12 && distance != 0) {
    digitalWrite(greenpin, LOW);
    digitalWrite(yellowpin, LOW);
    digitalWrite(redpin, HIGH);     
@@ -102,9 +111,21 @@ if (distance <= 9 && distance != 0) {
 
 } // End main loop
 
+/* Function to flash red light if too close. */
 void flashred(int d){
   digitalWrite(redpin, HIGH);
   delay(d);
   digitalWrite(redpin, LOW);    
 } // End flashred()
+
+/* Function to reset sensors if hung up. */
+void resetSensors(void){
+  pinMode(ECHO_PIN, OUTPUT);
+  pinMode(ECHO_PIN2, OUTPUT);  
+  digitalWrite(ECHO_PIN, LOW);
+  digitalWrite(ECHO_PIN2, LOW);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(ECHO_PIN2, INPUT); 
+  
+}
 
